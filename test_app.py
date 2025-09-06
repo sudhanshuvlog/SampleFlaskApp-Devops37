611529924f61feb0f778c86549dcb371b6e5f428
Trainersudhanshu <sudhanshutest123@gmail.com>
2025-09-06 14:23:14 +0530

    fixed the test cases

--- test_app.py
+++ test_app.py
@@ -24,3 +24,15 @@ def test_test(client):
     assert response.status_code == 200
     expected_text = 'test'
     assert expected_text.encode() in response.data
+
+def test_test1(client):
+    response = client.get('/test1')
+    assert response.status_code == 200
+    expected_text = 'test'
+    assert expected_text.encode() in response.data
+
+def test_test2(client):
+    response = client.get('/test2')
+    assert response.status_code == 200
+    expected_text = 'test'
+    assert expected_text.encode() in response.data

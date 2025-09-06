c39c894f1f39643bdf35bb5b6da9016c2de79d2b
Trainersudhanshu <sudhanshutest123@gmail.com>
2025-09-06 14:00:26 +0530

    updated

--- app.py
+++ app.py
@@ -3,16 +3,32 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    #x = 10
+    #y = x-10
     print("inside create_app function")
     
     @app.route('/')
     def home():
         print("inside home function")
+        test2()
         return 'Hurray Sudhanshu Updated1233333455555555!'
 
     @app.route('/test')
     def test():
         return "test 123456789"
+
+    return app
+
+def test1():
+    print("inside test1 function")
+
+def test2():
+    test1()
+    print("inside test2 function")
+
+for i in range(5):
+    print("for loop")
+    i=i+1
 
 if __name__ == '__main__':
     app = create_app()

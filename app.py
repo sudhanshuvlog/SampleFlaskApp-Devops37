fd437e1daf3f4f9a29e985f64634b49374a50485
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2025-12-27 10:23:48 +0530

    Whitespace fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     print("inside create_app function")
-    
+
     @app.route('/')
     def home():
         print("inside home function")
@@ -13,11 +13,11 @@ def create_app():
     @app.route('/test')
     def test():
         return "test 1123456789 abcdefg79239279273927393923982938239239"
-        
+   
     @app.route('/test2')
     def test1():
         return "test 2"
-        
+
     @app.route('/test3')
     def test2():
         return "test 1234"
@@ -27,5 +27,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    
+
     app.run(host='0.0.0.0', port=80, debug=True)
